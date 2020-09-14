0dd9fdaf783f08d4ce56c44b073fc1675e21071e
Duilio97 <duilio.g@hotmail.it>
2020-09-14 18:03:50 +0200

    first commit

---
+++ app/src/main/java/com/example/ambienteconfinato/UserActivity2.java
@@ -0,0 +1,34 @@
+package com.example.ambienteconfinato;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class UserActivity2 extends AppCompatActivity {
+    Button btnLogOut;
+    FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+        btnLogOut = (Button) findViewById(R.id.btnLogOut);
+        btnLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                FirebaseAuth.getInstance().signOut();
+                Intent I = new Intent(UserActivity2.this, ActivityLogin.class);
+                startActivity(I);
+
+            }
+        });
+
+    }
+}
